42442516969d4ee4208aae46f2f355c40ae48732
Jesse <gib19014@byui.edu>
2021-05-01 14:43:22 -0600

    Complete week 2

--- js/fill-toc.js
+++ js/fill-toc.js
@@ -1,26 +1,28 @@
 /*
-* Copyright (c) 2021 Jesse Gibbons
-* All rights reserved
-* 
-* Note: I am using functional programming tricks to make it difficult for
-*       less experienced programmers to explain what I'm doing.
-*
-*      I also want to avoid object-oriented programming when possible in
-*      this course, just to get a challenge.
-*/
-
+ * Copyright (c) 2021 Jesse Gibbons
+ * All rights reserved
+ * 
+ * Note: I am using functional programming tricks to make it difficult for
+ *       less experienced programmers to explain what I'm doing.
+ *
+ *      I also want to avoid object-oriented programming when possible in
+ *      this course, just to get a challenge.
+  */
 data = [
-	{
-		section: "Block 1",
-		links: [
-			{
-				label: "Week 1",
-				url: "week01/index.html"
-			}
-		]
-	}
+  {
+    section: "Block 1",
+    links: [
+      {
+        label: "Week 1",
+        url: "week01/index.html"
+      },
+      {
+        label: "Week 2",
+        url: "week02/index.html"
+      }
+    ]
+  }
 ]
-
 makeli = document.createElement.bind(document, "li");
 makea = document.createElement.bind(document, "a");
 makediv = document.createElement.bind(document, "div");
@@ -28,29 +30,27 @@ makeh2 = document.createElement.bind(document, "h2");
 makeul = document.createElement.bind(document, "ul");
 makeTextNode = document.createTextNode.bind(document);
 query = document.querySelector.bind(document);
-
 function makeTOCEntry(link){
-	const item = makeli();
-	const anchor = makea();
-	anchor.setAttribute("href", `${link.url}`);
-	anchor.appendChild(makeTextNode(`${link.label}`));
-	item.appendChild(anchor);
-	return item;
+  const item = makeli();
+  const anchor = makea();
+  anchor.setAttribute("href", `${link.url}`);
+  anchor.appendChild(makeTextNode(`${link.label}`));
+  item.appendChild(anchor);
+  return item;
 }
 function makeTOCSection(section){
-	const sect = makediv();
-	const sect_title = makeh2();
-	sect_title.appendChild(document.createTextNode(`${section.section}`));
-	sect.appendChild(sect_title);
-	const list = makeul();
-	section.links.forEach( (link) => list.appendChild(makeTOCEntry(link)));
-	sect.appendChild(list);
-	return sect;
+  const sect = makediv();
+  const sect_title = makeh2();
+  sect_title.appendChild(document.createTextNode(`${section.section}`));
+  sect.appendChild(sect_title);
+  const list = makeul();
+  section.links.forEach( (link) => list.appendChild(makeTOCEntry(link)));
+  sect.appendChild(list);
+  return sect;
 }
-	
 function makeTOC(toc, data)
 {
-	data.forEach((section) => toc.appendChild(makeTOCSection(section)));
+  data.forEach((section) => toc.appendChild(makeTOCSection(section)));
 }
 const toc = query("#contents");
 makeTOC(toc,data);
--- week02/quiz-ninja/main.js
+++ week02/quiz-ninja/main.js
@@ -1,1 +1,29 @@
-alert("Welcome to Quiz Ninja");
+const quiz = [
+  ["What is Superman's real name?", "Clark Kent"],
+  ["What is Wonder Woman's real name?", "Diana Prince"],
+  ["What is Batman's real name?", "Bruce Wayne"]
+];
+function start(quiz){
+  let score = 0;
+  for (const [question, answer] of quiz){
+    const response = ask(question);
+    score += check(response, answer)
+  }
+  gameOver()
+  function ask(question){
+    return prompt(question);
+  }
+  function check(response, answer){
+    if(response === answer){
+      alert(`correct!`);
+      return 1;
+    } else {
+      alert(`Wrong! the correct answer was ${answer}`);
+      return 0;
+    }
+  }
+  function gameOver(){
+    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+  }
+}
+start(quiz)
