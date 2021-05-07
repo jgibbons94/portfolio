9c520ca29ee1047b9fb673b211e36e0791aae801
Jesse <gib19014@byui.edu>
2021-05-08 00:52:45 +0100

    Do Novice to Ninja exercises in chapters 5, 6

--- week03/quiz-ninja/main.js
+++ week03/quiz-ninja/main.js
@@ -1,29 +1,53 @@
 const quiz = [
-  ["What is Superman's real name?", "Clark Kent"],
-  ["What is Wonder Woman's real name?", "Diana Prince"],
-  ["What is Batman's real name?", "Bruce Wayne"]
+  { name: "Superman", realName: "Clark Kent" },
+  { name: "Wonder Woman", realName: "Diana Prince" },
+  { name: "Batman", realName: "Bruce Wayne" }
 ];
-function start(quiz){
-  let score = 0;
-  for (const [question, answer] of quiz){
-    const response = ask(question);
-    score += check(response, answer)
-  }
-  gameOver()
-  function ask(question){
-    return prompt(question);
-  }
-  function check(response, answer){
-    if(response === answer){
-      alert(`correct!`);
-      return 1;
-    } else {
-      alert(`Wrong! the correct answer was ${answer}`);
-      return 0;
+const game = {
+  start(quiz){
+    //Ok, I'll admit, spreading is a new concept.
+    //But it's only an expressivity thing.
+    this.questions = [...quiz];
+    this.score = 0;
+
+    for(const question of this.questions){
+      this.question = question
+      this.ask();
     }
-  }
-  function gameOver(){
-    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    this.gameOver();
+  },
+  ask(){
+    const question = `What is ${this.question.name}'s real name?`;
+    view.render(view.question, question);
+    const response = prompt(question);
+    this.check(response);
+  },
+  check(response){
+    const answer = this.question.realName;
+    if(response === answer){
+      view.render(view.result, 'Correct!', {'class':'correct'});
+      alert('Correct!');
+      this.score++;
+      view.render(view.score, this.score);
+    } else {
+      view.render(view.result, `Wrong! The correct answer was ${answer}`, {'class':'wrong'});
+      alert(`Wrong! The correct answer was ${answer}`);
+    }
+  },
+  gameOver(){
+    view.render(view.info, `Game Over, you scored ${this.score} point${this.score != 1? 's' : ''}`);
   }
 }
-start(quiz)
+const view = {
+  score: document.querySelector('#score strong'),
+  question: document.getElementById('question'),
+  result: document.getElementById('result'),
+  info: document.getElementById('info'),
+  render(target, content, attributes) {
+    for(const key in attributes) {
+      target.setAttribute(key, attributes[key]);
+    }
+    target.innerHTML = content;
+  }
+};
+game.start(quiz);
