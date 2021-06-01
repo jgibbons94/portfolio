98cdca631176f34a8eaee836ee2a79b280f7b8ef
Jesse <gib19014@byui.edu>
2021-06-01 11:54:06 -0600

    Remove hikes model

--- team/07/hikes-model.js
+++
@@ -1,6 +0,0 @@
-const hikesCommentKey = "Hikes Comment";
-const getAllComments = () => JSON.parse(localStorage.getItem(hikesCommentKey));
-const comments = getAllComments();
-const getHikeComments = hikeName => comments[hikeName];
-const saveHikeComment = () => localStorage.setItem(hikesCommentKey, JSON.stringify(comments));
-export default hikesModel { getHikeComment, saveHikeComment};
